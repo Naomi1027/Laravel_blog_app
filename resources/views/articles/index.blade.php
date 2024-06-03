932efd51497e4aee04cf3c9aaadcb4f25793c78d
NAOMI MOTOMURA <naomimotomura@NAOMIsPC.local>
2024-06-03 13:13:29 +0900

    [feat #1] 記事一覧の初期ページを実装

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+    <article class="border border-black mb-8">
+        <span>yyyy年mm月dd日</span>
+        <h2>タイトル</h2>
+        <p>説明文</p>
+    </article>
+    <article>
+        <span>yyyy年mm月dd日</span>
+        <h2>タイトル</h2>
+        <p>説明文</p>
+    </article>
+    <article>
+        <span>yyyy年mm月dd日</span>
+        <h2>タイトル</h2>
+        <p>説明文</p>
+    </article>
+    <article>
+        <span>yyyy年mm月dd日</span>
+        <h2>タイトル</h2>
+        <p>説明文</p>
+    </article>
+</div>
+
+<a href="#" class="text-center">投稿する</a>
+@endsection
