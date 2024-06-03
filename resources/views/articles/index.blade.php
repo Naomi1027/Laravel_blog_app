d6ae3415a16b445480a12e9cfd88b08692cb859f
NAOMI MOTOMURA <naomimotomura@NAOMIsPC.local>
2024-06-03 15:16:50 +0900

    [feat #1] 記事一覧画面のデザインコーディング

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,28 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div>
-    <article class="border border-black mb-8">
-        <span>yyyy年mm月dd日</span>
-        <h2>タイトル</h2>
-        <p>説明文</p>
+
+<div class="flex flex-col gap-2">
+    @foreach ( [1, 2, 3, 4] as $index )
+    <article class="w-full rounded-lg border-gray-400 bg-stone-100 p-4 border-2">
+        <span class="text-xs mb-4 inline-block">yyyy年mm月dd日</span>
+        <h2 class="text-md font-bold mb-1">タイトル</h2>
+        <p class="text-md">説明文</p>
     </article>
-    <article>
-        <span>yyyy年mm月dd日</span>
-        <h2>タイトル</h2>
-        <p>説明文</p>
-    </article>
-    <article>
-        <span>yyyy年mm月dd日</span>
-        <h2>タイトル</h2>
-        <p>説明文</p>
-    </article>
-    <article>
-        <span>yyyy年mm月dd日</span>
-        <h2>タイトル</h2>
-        <p>説明文</p>
-    </article>
+    @endforeach
 </div>
 
-<a href="#" class="text-center">投稿する</a>
+<div class="text-center mt-4">
+    <a href="#" class="w-fit bg-cyan-400 p-2 inline-block tracking-normal text-white font-bold">投稿する</a>
+</div>
+
 @endsection
