f74b607c3dfaeb551d645d92dea86ce3a8f1863e
NAOMI MOTOMURA <naomimotomura@NAOMIsPC.local>
2024-06-04 12:09:17 +0900

    [fix #1] pint修正

--- database/seeders/ArticleSeeder.php
+++ database/seeders/ArticleSeeder.php
@@ -3,9 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Article;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
 
 class ArticleSeeder extends Seeder
 {
