1456f14d28d78a8cb130e71671546b420fb5ca41
Naomi1027 <guerrillawish@hotmail.com>
2024-06-05 18:01:36 +0900

    articleIdに数字だけ受け取れるよう修正

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
-Route::get('/articles/{articleId}', [ArticleController::class, 'show'])->name('articles.show');
+Route::get('/articles/{articleId}', [ArticleController::class, 'show'])->where('articleId', '[0-9]+')->name('articles.show');
