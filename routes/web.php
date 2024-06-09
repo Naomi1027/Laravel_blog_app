6d8945e8e1612b14e89808baacce2579a91b105f
Naomi1027 <guerrillawish@hotmail.com>
2024-06-09 22:02:57 +0900

    ルーティングのURLが間違っていたので修正

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('/articles/{articleId}', [ArticleController::class, 'show'])->where('articleId', '[0-9]+')->name('articles.show');
-Route::get('/article/create', [ArticleController::class, 'create'])->name('articles.create');
+Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
 Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
