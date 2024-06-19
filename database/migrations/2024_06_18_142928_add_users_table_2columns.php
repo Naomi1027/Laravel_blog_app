39e3eaa165f8cd2497508b693c0fbc360d6b7442
Naomi1027 <guerrillawish@hotmail.com>
2024-06-19 11:54:19 +0900

    [fix]カラムをnullableに変更

--- database/migrations/2024_06_18_142928_add_users_table_2columns.php
+++ database/migrations/2024_06_18_142928_add_users_table_2columns.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('icon_path', 255)->after('password');
+            $table->string('icon_path', 255)->nullable();
             $table->softDeletes();
         });
     }
