1efe686efe26b4a79446744e862e691641c2f505
Naomi1027 <guerrillawish@hotmail.com>
2024-06-19 09:10:24 +0900

    [feat]カラムのホワイトリスト追加とリレーションの追加

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Article extends Model
@@ -14,5 +15,11 @@ class Article extends Model
     protected $fillable = [
         'title',
         'content',
+        'user_id',
     ];
+
+    public function user():BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
