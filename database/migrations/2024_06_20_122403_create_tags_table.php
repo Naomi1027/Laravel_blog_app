8efc305b0199d55ca2ce74ba6450f5662b246ad4
Naomi1027 <guerrillawish@hotmail.com>
2024-06-24 15:49:21 +0900

    [fix]unique制約の追加

--- database/migrations/2024_06_20_122403_create_tags_table.php
+++ database/migrations/2024_06_20_122403_create_tags_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 255);
-            $table->string('key', 255);
+            $table->string('name', 255)->unique();
+            $table->string('key', 255)->unique();
             $table->timestamps();
         });
     }
