90e87ab6a26f32456f35c1849a23013edfb8a668
Naomi1027 <guerrillawish@hotmail.com>
2024-07-02 13:23:47 +0900

    [fix]レイアウト修正

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -2,11 +2,10 @@
 
 <div class="mt-20 mx-auto w-full">
     <article class="flex">
-        @foreach ($articles as $article)
         <div class="w-40">
             <img src="{{ $article->user->icon_path }}" alt="" class="w-24 h-24 rounded-full">
         </div>
-        <div>
+        <div class="w-full">
             <div class="flex relative">
                 <h2 class="mb-6 text-2xl">{{ $article->user->display_name }}</h2>
                 <div class="absolute top-0 right-0">
@@ -24,27 +23,24 @@
             </div>
             <h1 class="mb-6 text-3xl">{{ $article->title }}</h1>
             <div>
-                    @if ( $article->tags()->exists())
-                    @foreach ( $article->tags as $tag )
-                    <p class="text-xm mb-4 mr-4 p-2 rounded bg-gray-300 inline-block">{{ $tag->name }}</p>
-                    @endforeach
-                    @endif
-                    <div class="mb-4 text-base flex gap-3 text-gray-600">
-                        <div>
-                            <span class="mr-1">最終更新日</span>
-                            <span>{{ $article->updated_at->format('Y年m月d日') }}</span>
-                        </div>
-                        <div>
-                            <span class="mr-1">投稿日</span>
-                            <span>{{ $article->created_at->format('Y年m月d日') }}</span>
-                        </div>
+                @if ( $article->tags()->exists())
+                @foreach ( $article->tags as $tag )
+                <p class="text-xm mb-4 mr-4 p-2 rounded bg-gray-300 inline-block">{{ $tag->name }}</p>
+                @endforeach
+                @endif
+                <div class="mb-4 text-base flex gap-3 text-gray-600">
+                    <div>
+                        <span class="mr-1">最終更新日</span>
+                        <span>{{ $article->updated_at->format('Y年m月d日') }}</span>
                     </div>
                     <div>
+                        <span class="mr-1">投稿日</span>
+                        <span>{{ $article->created_at->format('Y年m月d日') }}</span>
+                    </div>
                 </div>
             </div>
             <p class="mb-20">{{ $article->content }}</p>
         </div>
-        @endforeach
     </article>
     @foreach ( $article->comments as $comment )
     <section class="border-2 border-gray-400 mt-4 p-8">
