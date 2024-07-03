117ed2b87b7d4b61f5d9fb9665172b1d7b8f821c
Naomi1027 <guerrillawish@hotmail.com>
2024-07-04 00:10:28 +0900

    [fix]ルーティングのエンドポイント修正

--- routes/web.php
+++ routes/web.php
@@ -18,18 +18,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('/{userName}/articles/{articleId}', [ArticleController::class, 'show'])->where('articleId', '[0-9]+')->name('articles.show');
-Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
-Route::post('/articles/{articleId}', [ArticleController::class, 'update'])->name('articles.update');
-Route::delete('/articles/{articleId}', [ArticleController::class, 'destroy'])->name('articles.destroy');
 
 Route::group(['middleware' => ['auth']], function () {
     Route::get('/articles/create', [ArticleController::class, 'create'])->name('articles.create');
     Route::get('/{userName}/articles/{articleId}/edit', [ArticleController::class, 'edit'])->where('articleId', '[0-9]+')->name('articles.edit');
-    Route::get('/{articleId}/comments/{commentId}/edit', [CommentController::class, 'edit'])->where(['articleId' => '[0-9]+', 'commentId' => '[0-9]+'])->name('comments.edit');
+    Route::post('/articles', [ArticleController::class, 'store'])->name('articles.store');
+    Route::post('/articles/{articleId}', [ArticleController::class, 'update'])->name('articles.update');
+    Route::delete('/articles/{articleId}', [ArticleController::class, 'destroy'])->name('articles.destroy');
+    Route::post('/{userName}/articles/{articleId}/comments', [CommentController::class, 'store'])->name('comments.store');
+    Route::post('/comments/{commentId}/', [CommentController::class, 'update'])->name('comments.update');
+    Route::get('/comments/{commentId}/edit', [CommentController::class, 'edit'])->where('commentId', '[0-9]+')->name('comments.edit');
 });
-
-Route::post('/{articleId}/comments/store', [CommentController::class, 'store'])->name('comments.store');
-Route::post('/comments/{commentId}/update', [CommentController::class, 'update'])->name('comments.update');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
