dac9a6087acf2d51b10387b4409ca3f20befcce1
Naomi1027 <guerrillawish@hotmail.com>
2024-07-18 14:17:31 +0900

    [feat]投稿画面にタグのチェックボックス追加

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -12,6 +12,17 @@
                 @enderror
             </div>
             <div class="mb-6">
+                <p class="pr-8">タグの選択</p>
+                    @foreach ($tags as $tagId => $tagName)
+                        <input type="checkbox" id="{{ $tagName}}" name="tags[]" value="{{ $tagId }}" @if(is_array(old('tags')) && in_array($tagId, old('tags'))) checked @endif>
+                        <label for="{{ $tagName }}" class="text-xl pr-2">{{ $tagName }}</label>
+                    @endforeach
+                    @error('tags')
+                            <p class="text-red-700">{{ $message }}</p>
+                    @enderror
+            </div>
+
+            <div class="mb-6">
                 <label for="content">本文</label>
                 <textarea name="content" id="content" rows="15" class="w-full border-solid border-2 p-2 text-xl">{{ old('content') }}</textarea>
                 @error('content')
