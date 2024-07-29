5abdf6def0d1e84d2b0d09838c6ad3a2ca895224
Naomi1027 <guerrillawish@hotmail.com>
2024-07-29 09:22:22 +0900

    [feat]likeメソッドの実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use App\Http\Requests\StoreArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
 use App\Models\Article;
 use App\Models\Tag;
+use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -19,7 +20,7 @@ class ArticleController extends Controller
     public function index(Request $request): View
     {
         $keyword = $request->input('keyword');
-        $articles = Article::with('user', 'tags')->when($keyword, function ($query, $keyword) {
+        $articles = Article::with('user', 'tags', 'userLikes')->when($keyword, function ($query, $keyword) {
             $query->where('title', 'like', "%{$keyword}%")
                 ->orWhere('content', 'like', "%{$keyword}%");
         })->latest()->paginate(10);
@@ -119,5 +120,28 @@ class ArticleController extends Controller
         $article->tags()->detach();
 
         return redirect()->route('articles.index');
+    }
+
+    /**
+     * 引数のarticleIdに紐づくarticleにLIKEする
+     *
+     * @param int $articleId
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function like(int $articleId): RedirectResponse
+    {
+        $authUser = User::find(Auth::id());
+        $article = Article::find($articleId);
+
+        // ログインユーザーがいいねした記事をすべて取得し、article_idカラムといいねしたい記事のidが存在するかを判定
+        if ($authUser->articleLikes()->where('article_id', $articleId)->exists()) {
+            // 記事にいいねしていたのが解除される
+            $authUser->articleLikes()->detach($articleId);
+        } else {
+            // 記事にいいねしたのがlikesテーブルにレコードが追加される
+            $authUser->articleLikes()->attach($articleId);
+        }
+
+        return redirect()->route('articles.show', ['userName' => $article->user->name, 'articleId' => $articleId]);
     }
 }
