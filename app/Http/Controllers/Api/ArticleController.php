66a1aadec36d83907c777433c32f92b0c92afc50
Naomi1027 <guerrillawish@hotmail.com>
2024-08-05 21:27:47 +0900

    [fix]ArticleControllerのindexメソッド修正

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -11,9 +11,16 @@ class ArticleController extends Controller
 {
     /**
      * 全ての記事を取得するFunction
+     *
+     * @return JsonResource
      */
     public function index(): JsonResource
     {
-        return ArticleResource::collection(Article::with('user', 'tags', 'userLikes')->latest()->orderBy('id', 'ASC')->paginate(10));
+        /**
+         * @var Article $articleLists
+         */
+        $articleLists = Article::with('user', 'tags', 'userLikes')->latest()->orderBy('id', 'ASC')->paginate(10);
+
+        return ArticleResource::collection($articleLists);
     }
 }
