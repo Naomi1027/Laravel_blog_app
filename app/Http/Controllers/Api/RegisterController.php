010431f287c43f3277b46d0768037473effc6ff8
Naomi1027 <guerrillawish@hotmail.com>
2024-08-13 12:05:43 +0900

    [feat]RegisterControllerのregisterメソッド作成

---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Symfony\Component\HttpFoundation\Response;
+
+class RegisterController extends Controller
+{
+    /**
+     * 新しいユーザーを登録
+     *
+     * @param RegisterRequest $request
+     * @return Response
+     */
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $validated['password'] = bcrypt($request->password);
+        $user = User::create($validated);
+        event(new Registered($user));
+
+        return response()->json([
+            'message' => 'Registered!',
+            'name' => $request->name,
+            'email' => $request->email,
+        ], Response::HTTP_OK);
+    }
+}
