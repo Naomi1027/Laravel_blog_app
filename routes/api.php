822c90c110822072e8944e4a1c0f6510ff6909b1
Naomi1027 <guerrillawish@hotmail.com>
2024-08-13 12:02:46 +0900

    [feat]ユーザー登録とメール認証のルーティング追加

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\VerifyEmailController;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +21,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [RegisterController::class, 'register'])->name('register');
+
+Route::post('/email/verify/{id}/{hash}', [VerifyEmailController::class, 'verifyEmail'])->name('verification.verify');
 
 Route::get('/articles', [ArticleController::class, 'index']);
