38a204473d9cafe771f9af422f3a9657913efe39
Naomi1027 <guerrillawish@hotmail.com>
2024-08-20 15:53:03 +0900

    [fix]パスカケースに変更とAPIディレクトリ作成

--- app/Http/Controllers/Api/RegisterController.php
+++ app/Http/Controllers/Api/RegisterController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\API\RegisterRequest;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
 use Symfony\Component\HttpFoundation\Response;
