e922333b4a5d56e327b81fe4a7676fb56b325989
Naomi1027 <guerrillawish@hotmail.com>
2024-08-21 00:49:26 +0900

    [fix]larastanの修正

--- app/Http/Controllers/Api/VerifyEmailController.php
+++ app/Http/Controllers/Api/VerifyEmailController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\API\EmailVerificationRequest;
+use App\Http\Requests\Api\EmailVerificationRequest;
 use App\Models\User;
 use Symfony\Component\HttpFoundation\Response;
 
