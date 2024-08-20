5797020d0847f1704d1d3f1836bea62fd8fb8014
Naomi1027 <guerrillawish@hotmail.com>
2024-08-20 10:08:20 +0900

    [feat]LogoutController作成

---
+++ app/Http/Controllers/Api/LogoutController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogoutController extends Controller
+{
+    /**
+     * ログアウト処理を行うFunction
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function logout(Request $request): Response
+    {
+        if (Auth::check() === false) {
+            return response()->json([
+                'message' => 'ログインしていません!',
+            ], Response::HTTP_UNAUTHORIZED);
+        } else {
+            Auth::logout();
+            $request->session()->invalidate();
+            $request->session()->regenerateToken();
+
+            return response()->json([
+                'message' => 'ログアウトしました!',
+            ], Response::HTTP_OK);
+        }
+    }
+}
