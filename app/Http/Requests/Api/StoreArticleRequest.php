c53720952d457b8ad2a69580fd69791fd0e2a935
Naomi1027 <guerrillawish@hotmail.com>
2024-08-23 15:49:41 +0900

    [feat]StoreArticleRequest作成

---
+++ app/Http/Requests/Api/StoreArticleRequest.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreArticleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:255'],
+            'content' => ['required', 'string'],
+            'tags' => ['nullable', 'array', 'max:3'],
+            'tags.*' => ['integer', 'exists:tags,id'],
+        ];
+    }
+
+    /**
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'title.required' => 'タイトルは必須です。',
+            'title.string' => 'タイトルは文字列にして下さい。',
+            'title.max' => 'タイトルは:max文字以内でお願いします。',
+            'content.required' => '本文は必須です。',
+            'content.string' => '本文は文字列にして下さい。',
+            'tags.max' => 'タグは:maxつまで選択して下さい',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator): HttpResponseException
+    {
+        $response = [
+            'status' => 422,
+            'message' => '適切な値を入力してください',
+            'errors' => $validator->errors(),
+        ];
+
+        throw new HttpResponseException(response()->json($response, 422));
+    }
+}
