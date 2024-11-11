598a62e0c68d4300419884ad9c3caf9f408eed5b
Naomi1027 <guerrillawish@hotmail.com>
2024-11-11 15:15:22 +0900

    [update]パスの修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,8 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\UrlGenerator;
-use Illuminate\Support\Facades\Artisan;
-use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,10 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if (!File::exists(public_path('storage'))) {
-            Artisan::call('storage:link');
-        }
-
         if (config('app.env') !== 'local') {
             $url->forceScheme('https');
         }
