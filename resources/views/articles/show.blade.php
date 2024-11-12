1405d5e3aca139196c25fd6a517f246275ae262a
Naomi1027 <guerrillawish@hotmail.com>
2024-11-12 09:40:55 +0900

    [update]show.blade.phpの修正

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,7 +9,7 @@
     <article class="flex">
         <div>
             <div class="w-40">
-                <img src="/storage/images/{{ $article->user->icon_path }}" alt="" class="w-24 h-24 rounded-full">
+                <img src="{{ asset('images/' . $article->user->icon_path) }}" alt="アイコン" class="w-24 h-24 rounded-full" />
             </div>
             <div class="flex mt-4 ml-6">
                 @if ($article->userLikes->contains($authUser))
