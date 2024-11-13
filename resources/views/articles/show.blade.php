a9c166fa36a948f22f0e7a1a24af55b79eecea67
Naomi1027 <guerrillawish@hotmail.com>
2024-11-13 22:18:31 +0900

    [update]アイコンの表示を修正

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -9,7 +9,12 @@
     <article class="flex">
         <div>
             <div class="w-40">
-                <img src="{{ $article->user->icon_path) }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+                {{-- アイコン --}}
+                @if ($article->user->icon_path === null)
+                    <img src="{{ asset('/images/user_default.png') }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+                @else
+                    <img src="{{ $article->user->icon_path }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+                @endif
             </div>
             <div class="flex mt-4 ml-6">
                 @if ($article->userLikes->contains($authUser))
@@ -66,7 +71,14 @@
     <section class="border-2 border-gray-400 mt-4 p-8">
         <h3 class="text-2xl ml-4 mb-4">コメント</h3>
         <div class="flex mb-4">
-            <img src="{{ $comment->user->icon_path) }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+            {{-- アイコン --}}
+            @if ($comment->user->icon_path === null)
+                <img src="{{ asset('/images/user_default.png') }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+            @else
+                <img src="{{ $comment->user->icon_path }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+            @endif
+
+            {{-- <img src="{{ $comment->user->icon_path) }}" alt="アイコン" class="w-24 h-24 rounded-full" /> --}}
             <h2 class="mb-6 ml-4 text-2xl">{{ $comment->user->display_name }}</h2>
             @if (Auth::id() === $comment->user_id)
             <div class="flex-col space-y-2 ml-auto">
@@ -86,7 +98,12 @@
     @auth
     <section class="border-2 border-gray-400 mt-4 p-8">
         <div class="flex mb-4">
-            <img src="{{ $authUser->icon_path) }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+            @if ($authUser->icon_path === null)
+                <img src="{{ asset('/images/user_default.png') }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+            @else
+                <img src="{{ $authUser->icon_path) }}" alt="アイコン" class="w-24 h-24 rounded-full" />
+            @endif
+            {{-- <img src="{{ $authUser->icon_path }}" alt="アイコン" class="w-24 h-24 rounded-full" /> --}}
             <h3 class="text-2xl ml-4 mb-4">コメントする</h3>
         </div>
         <form method="POST" action="{{ route('comments.store', ['userName' => $article->user->name, 'articleId' => $article->id]) }}">
