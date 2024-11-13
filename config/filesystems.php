9d7389b34ee3e67efd820912ac8e3f7b8cb48196
Naomi1027 <guerrillawish@hotmail.com>
2024-11-13 19:21:44 +0900

    [update]filesystems.phpを修正

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,6 +36,13 @@ return [
             'throw' => false,
         ],
 
+        // 'direct' => [
+        //     'driver' => 'local',
+        //     'root' => public_path('images'),  // 'images'ディレクトリを指定
+        //     'url' => env('APP_URL') . '/images',  // 正しいURLパスに修正
+        //     'visibility' => 'public',
+        // ],
+
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
@@ -50,10 +57,10 @@ return [
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
-            'url' => env('APP_URL'),
+            'url' => env('AWS_URL'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
-            'throw' => false,
+            'throw' => true,
         ],
 
     ],
