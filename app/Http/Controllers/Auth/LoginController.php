eb5c766d54fb8152b6df790354fefd8475ca499f
Naomi1027 <guerrillawish@hotmail.com>
2024-11-19 11:57:07 +0900

    [add]Googleアカウントでログインするための処理を追加

---
+++ app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class LoginController extends Controller
+{
+    public function redirectToGoogle()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function handleGoogleCallback()
+    {
+        $googleUser = Socialite::driver('google')->stateless()->user();
+
+        $user = User::updateOrCreate([
+            'email' => $googleUser->getEmail(),
+        ], [
+            'name' => $googleUser->getName(),
+            'email' => $googleUser->getEmail(),
+        ]);
+
+        // ユーザーをログインさせる
+        Auth::login($user, true);
+
+        // ホームページへリダイレクト
+        return redirect('/dashboard');
+    }
+}
