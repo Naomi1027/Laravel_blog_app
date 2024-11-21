d0cce15e776c1c4a15b5bcadd46ff7fc8288dce3
Naomi1027 <guerrillawish@hotmail.com>
2024-11-21 10:34:27 +0900

    [update]アイコンのバリデーションを修正

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -19,11 +19,7 @@ class ProfileUpdateRequest extends FormRequest
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'display_name' => ['nullable', 'string', 'max:255'],
-            'icon_path' => [
-                'nullable',                // 必須ではない
-                'mimes:jpeg,jpg,png,gif', // 許可される画像形式を指定
-                'max:2048',               // ファイルサイズの上限（ここでは2MB）
-            ],
+            'icon_path' => ['nullable'],
         ];
     }
 }
