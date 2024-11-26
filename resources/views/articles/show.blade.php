fb6e3881b359f6e9b601b83da48ee3ab6dd22517
Naomi1027 <guerrillawish@hotmail.com>
2024-11-26 15:10:24 +0900

    [update]画像ファイルのパスを修正

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -69,7 +69,7 @@
             {{-- 投稿画像を表示 --}}
             @if ( $article->image )
             <div class="flex justify-center mb-6">
-                <img src="{{ asset($article->image) }}" alt="画像" class="w-full max-w-2xl h-auto rounded-lg shadow-md" />
+                <img src="{{ Storage::disk('s3')->url("$article->image")}}" alt="画像" class="w-full max-w-2xl h-auto rounded-lg shadow-md" />
             </div>
             @endif
         </div>
